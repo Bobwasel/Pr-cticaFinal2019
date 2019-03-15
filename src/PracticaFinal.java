a6b295bc5075ce072e6054bcf4b33b8c036f07cd
Leroy Merlos <leroymerlos@gmail.com>
2019-03-15 17:14:48 +0100

    primer commit de prueba

---
+++ src/PracticaFinal.java
@@ -0,0 +1,6 @@
+public class PracticaFinal {
+
+    public static void main (String[] args){
+        System.out.println("Y con esta práctica nos despedimos de la vida social, adiós :D!");
+    }
+}
